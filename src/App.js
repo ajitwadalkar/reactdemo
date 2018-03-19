e90b23554090a132795e3130765699a8d8fd336a
ajitwadalkar <ajitwadalkar@gmail.com>
2018-03-19 17:26:12 -0400

    updated dependancies

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
 import React, { Component } from 'react';
 import logo from './moneymanager.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
+    handleSubmit(e){
+        console.log(this.refs.password.value);
+        axios.post('/user/login', {
+            username: this.refs.userName.value,
+            password: this.refs.password.value
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        e.preventDefault();
+    }
+    render() {
     return (
-      <div className="App">
+      <div className="App"> <br/><br/><br/><br/><br/><br/>
+          <div id="loginbox"   >
+              <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <span className="App-header-span"> MoneyManager </span>
+                  <img src={logo} className="App-logo" alt="logo" />
+              </header>
 
-          <br/><br/><br/><br/><br/><br/>
-            <div id="loginbox"   >
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <span className="App-header-span"> MoneyManager </span>
-                    <img src={logo} className="App-logo" alt="logo" />
-                </header>
-                <div className="loginboxdata">
-                    <b className="loginboxdata-u">User Name :</b>
-                    <br />
-                    <input type="text" id="unm" onfocus="this.removeAttribute('readonly');" readonly /><br />
-                    <b className="loginboxdata-p">Password :</b>
-                    <br />
-                    <input type="password"    id="pwd" onfocus="this.removeAttribute('readonly');" readonly/><br />
-                    <br />
-                    <input id="Submit1" type="submit"    onserverclick="submit_click" className="mybuttonstyle" value="Sign in" /><br /><br /><br />
-                    New User? <a href="#">Sign Up!!!</a> Here
-
-
-                </div>
-            </div>
-
+              <div className="loginboxdata">
+                  <form onSubmit = {this.handleSubmit.bind(this)}>
+                      <b className="loginboxdata-u" >User Name :</b>
+                      <br />
+                      <input type="text" id="unm"  ref="userName"/><br />
+                      <b className="loginboxdata-p">Password :</b>
+                      <br />
+                      <input type="password" id="pwd" ref="password" /><br />
+                      <br />
+                      <input id="Submit1" type="submit"   className="mybuttonstyle" value="Sign in" />
+                      <br /><br /><br />
+                  </form>
+                  New User? <a href="#">Sign Up!!!</a> Here
+              </div>
+          </div>
       </div>
     );
-  }
+    }
 }
 
 export default App;
